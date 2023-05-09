534d5c3ab0ffd6b06f2d9b33dd0611c5f3bfdcf8
Rizzzky <114666981+Rizzzky@users.noreply.github.com>
2023-05-09 20:23:19 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    System out println("Mochamad Rizky Fauzi");
+
     
   }
 }
